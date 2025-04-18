57742c6e70f551e17950323f1b77ae19af38a3b7
Yasser Vaz d'Almeida <yasilvalmeida@hotmail.com>
2025-04-18 13:34:31 +0100

    Replace pinata gateway url

--- src/libs/utils/pinata/index.ts
+++ src/libs/utils/pinata/index.ts
@@ -27,7 +27,7 @@ export const uploadFileToPinata = async (
       .keyvalues({
         uploadedBy: 'SwapForge',
       });
-    return `https://coffee-defensive-hare-118.mypinata.cloud/ipfs/${result.cid}`;
+    return `https://ipfs.swapforge.app/${result.cid}`;
   } catch (error) {
     console.log('Error uploading file to Pinata:', error);
     throw error;
